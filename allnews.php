75d5e16b85aa6667c53994c75348db9f39599ea0
daniyarmukhanov <daniyarmuslim@gmail.com>
2015-03-16 16:05:03 +0600

    limit b15

--- allnews.php
+++ allnews.php
@@ -14,7 +14,7 @@ require_once __DIR__ . '/db_connect.php';
 $db = new DB_CONNECT();
 
 // get all products from products table
-$result = mysql_query("SELECT * FROM  `news` ORDER BY  `news`.`time` DESC LIMIT 0 , 30") or die(mysql_error());
+$result = mysql_query("SELECT * FROM  `news` ORDER BY  `news`.`time` DESC LIMIT 0 , 15") or die(mysql_error());
 
 // check for empty result
 if (mysql_num_rows($result) > 0) {
@@ -31,6 +31,7 @@ if (mysql_num_rows($result) > 0) {
         $news["text"] = $row["text"];
         $news["time"] = $row["time"];
         $news["photo"] = $row["photo"];
+        $news["resource_id"] = $row["resource_id"];
 
         // push single product into final response array
         array_push($response["news"], $news);
